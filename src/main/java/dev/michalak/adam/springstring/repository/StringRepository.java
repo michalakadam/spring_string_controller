07ba9d1bf09b7dae9cd0807d8220f0d60f6f9527
Adam Michalak <adam.michalak.dev@gmail.com>
2021-12-21 09:55:42 +0100

    add missing repository annotation

--- src/main/java/dev/michalak/adam/springstring/repository/StringRepository.java
+++ src/main/java/dev/michalak/adam/springstring/repository/StringRepository.java
@@ -2,5 +2,7 @@ package dev.michalak.adam.springstring.repository;
 
 import dev.michalak.adam.springstring.repository.entity.PalindromeEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StringRepository extends CrudRepository<PalindromeEntity, Long> { }
